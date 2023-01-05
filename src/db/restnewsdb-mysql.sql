043ba445cfbdd2e4206a69d497d6a0e942cc8df2
yl111 <yuriy.lee.111@gmail.com>
2023-01-05 23:01:05 +0600

    Database Authentication

--- src/db/restnewsdb-mysql.sql
+++ src/db/restnewsdb-mysql.sql
@@ -49,3 +49,53 @@ INSERT INTO `comment`
 VALUES (1, 'aaa@mail.com', 'My first text', 'AAA name', 1),
        (2, 'bbb@mail.com', 'My second text', 'BBB name', 1),
        (3, 'ccc@mail.com', 'My third text', 'CCC name', 1);
+
+
+
+CREATE TABLE `user`
+(
+    `id`        bigint       NOT NULL AUTO_INCREMENT,
+    `email`     varchar(255) NOT NULL,
+    `full_name` varchar(255) NOT NULL,
+    `password`  varchar(255) NOT NULL,
+    `username`  varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`email`),
+    UNIQUE KEY (`username`)
+);
+
+
+CREATE TABLE `role`
+(
+    `id`   bigint NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `users_roles`
+(
+    `user_id` bigint NOT NULL,
+    `role_id` bigint NOT NULL,
+    PRIMARY KEY (`user_id`, `role_id`),
+    KEY (`role_id`),
+    CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+    CONSTRAINT FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+);
+
+
+INSERT INTO `restnewsdb`.`user`
+VALUES ('1', 'yuriy@mail.ru', 'yuriy', '$2a$10$SR8eHOrMIQqY.Ko9XTOpROvNoxkQrJndF4wuh/W6xkpmlZ5y.ywnO', 'yuriy'),
+       ('2', 'admin@mail.ru', 'admin', '$2a$10$nxXIjXm19N2KkYXfjHHm6e8qG2xpL1qqFEiiS42RYprx8Y3GdnB5e', 'admin');
+
+
+INSERT INTO `restnewsdb`.`role`
+VALUES ('1', 'ROLE_USER'),
+       ('2', 'ROLE_ADMIN');
+
+
+INSERT INTO `restnewsdb`.`users_roles`
+VALUES ('1', '1'),
+       ('2', '2');
+
+
---
+++ src/main/java/com/lee/restnews/entity/Role.java
@@ -0,0 +1,22 @@
+package com.lee.restnews.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
