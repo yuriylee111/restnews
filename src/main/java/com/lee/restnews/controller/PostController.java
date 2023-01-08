d2b4ca2d41113e68cb89b7a5c6823b3421eddd74
yl111 <yuriy.lee.111@gmail.com>
2023-01-08 13:52:32 +0600

    Add Versioning through URI Path

--- src/main/java/com/lee/restnews/controller/PostController.java
+++ src/main/java/com/lee/restnews/controller/PostController.java
@@ -2,6 +2,7 @@ package com.lee.restnews.controller;
 
 
 import com.lee.restnews.dto.PostDto;
+import com.lee.restnews.dto.PostDtoV2;
 import com.lee.restnews.dto.PostResponse;
 import com.lee.restnews.service.PostService;
 import jakarta.validation.Valid;
@@ -10,12 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.lee.restnews.util.Constant.*;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping
 public class PostController {
 
     private final PostService postService;
@@ -24,13 +26,13 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping
+    @PostMapping("/api/v1/posts")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping("/api/v1/posts")
     public PostResponse getAllPosts(
             @RequestParam(value = PAGE_NO_TEMPLATE, defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = PAGE_SIZE_TEMPLATE, defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize,
@@ -40,26 +42,44 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable(name = ID) Long id) {
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getPostByIdV1(@PathVariable(name = ID) Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
-    @PutMapping("/{id}")
+    @GetMapping("/api/v2/posts/{id}")
+    public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name = ID) Long id) {
+        PostDto postDto = postService.getPostById(id);
+        PostDtoV2 postDtoV2 = new PostDtoV2();
+        postDtoV2.setId(postDto.getId());
+        postDtoV2.setTitle(postDto.getTitle());
+        postDtoV2.setDescription(postDto.getDescription());
+        postDtoV2.setContent(postDto.getContent());
+
+        List<String> tags = new ArrayList<>();
+        tags.add("Java");
+        tags.add("Spring Boot");
+        tags.add("AWS");
+        postDtoV2.setTags(tags);
+
+        return ResponseEntity.ok(postDtoV2);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = ID) Long id) {
         PostDto postResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deletePost(@PathVariable(name = ID) Long id) {
         postService.deletePostById(id);
         return new ResponseEntity<>("Post entity deleted successfully!", HttpStatus.OK);
     }
 
-    @GetMapping("/category/{id}")
+    @GetMapping("/api/v1/posts/category/{id}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable("id") Long categoryId) {
         List<PostDto> postDtos = postService.getPostsByCategory(categoryId);
         return ResponseEntity.ok(postDtos);
---
+++ src/main/java/com/lee/restnews/dto/PostDtoV2.java
@@ -0,0 +1,27 @@
+package com.lee.restnews.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class PostDtoV2 {
+
+    private Long id;
+
+    @Size(min = 2, message = "Post title should have at least 2 characters")
+    private String title;
+
+    @Size(min = 10, message = "Post description should have at least 10 characters")
+    private String description;
+
+    @NotEmpty
+    private String content;
+
+    private Set<CommentDto> comments;
+    private Long categoryId;
+    private List<String> tags;
+}
